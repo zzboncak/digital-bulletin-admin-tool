8d899ab54f8e0c1b403b4dc66803ae56618405d4
zzboncak <zachary.zboncak@gmail.com>
2021-06-28 17:42:22 -0500

    initial commit 💩

---
+++ tests/main.ts
@@ -0,0 +1,21 @@
+import { Meteor } from 'meteor/meteor';
+import assert from 'assert';
+
+describe('digital-bulletin-admin-tool', function () {
+  it('package.json has correct name', async function () {
+    const { name } = await import('../package.json');
+    assert.strictEqual(name, 'digital-bulletin-admin-tool');
+  });
+
+  if (Meteor.isClient) {
+    it('client is not server', function () {
+      assert.strictEqual(Meteor.isServer, false);
+    });
+  }
+
+  if (Meteor.isServer) {
+    it('server is not client', function () {
+      assert.strictEqual(Meteor.isClient, false);
+    });
+  }
+});
