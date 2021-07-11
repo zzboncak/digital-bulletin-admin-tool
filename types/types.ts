84e0852e7a359be7580297a3c8461f3052347049
zzboncak <zachary.zboncak@gmail.com>
2021-07-11 13:19:01 -0500

    added grapher package and subsection interface

--- types/types.ts
+++ types/types.ts
@@ -1,10 +1,23 @@
-export type ServiceElemetTypes = "song" | "liturgy" | "hymn" | "sermon" | "announcement" | "scripture";
+export type ServiceElemetTypes =
+  | "song"
+  | "liturgy"
+  | "hymn"
+  | "sermon"
+  | "announcement"
+  | "scripture";
 
 export interface ServiceElement {
-    _id?: string;
-    title: string;
-    isExpandable: boolean;
-    type: ServiceElemetTypes,
-    content: string;
-    createdAt: Date;
+  _id?: string;
+  title: string;
+  isExpandable: boolean;
+  type: ServiceElemetTypes;
+  content: string;
+  createdAt: Date;
+}
+
+export interface Subsection {
+  _id?: string;
+  title: string;
+  subtext: string;
+  serviceElements: string[];
 }
