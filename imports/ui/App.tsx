a35549a4fc8f6bd4b379a4c71c97816bb152553a
zzboncak <zachary.zboncak@gmail.com>
2021-08-24 17:17:42 -0500

    nested app stuff in switch and basic routes

--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -2,19 +2,27 @@ import { Meteor } from "meteor/meteor";
 import React from "react";
 import { Login } from "./Login";
 import { useTracker } from "meteor/react-meteor-data";
+import { Switch, Route, Link } from "react-router-dom";
 
 export const App = () => {
   const user = useTracker(() => Meteor.user());
 
   return (
-    <div>
-      {!!user && (
-        <>
-          <h1>Welcome to Meteor, Nate!</h1>
-          <button onClick={() => Meteor.logout()}>Log out</button>
-        </>
-      )}
-      {!user && <Login />}
-    </div>
+    <Switch>
+      <Route path="/login">
+        <Login />
+      </Route>
+      <Route exact path="/">
+        <div>
+          {!!user && (
+            <>
+              <h1>Welcome to Meteor, Nate!</h1>
+              <button onClick={() => Meteor.logout()}>Log out</button>
+            </>
+          )}
+          {!user && <Link to="/login">Login</Link>}
+        </div>
+      </Route>
+    </Switch>
   );
 };
